c5cacb4ef5870b597928637bc49a16231bb08229
Curtis Maves <cmaves@purdue.edu>
2018-01-20 19:10:48 +0000

    Added better comments to code

--- weightspec.py
+++ weightspec.py
@@ -26,29 +26,37 @@ class Strip1(animation.BaseStripAnim):
 class Spectrum(animation.BaseMatrixAnim):
 	def __init__(self, led, width = 12, height = 4):
 		super(Spectrum, self).__init__(led)
+		#setup audio input stream from Pulseaudio ALSA
 		self.istream = aa.PCM(type=aa.PCM_CAPTURE, mode=aa.PCM_NORMAL, device='pulse')
-		self.chunk = 2048
-		self.istream.setperiodsize(self.chunk)
+		self.chunk = 2048 #<- This is how many samples it collects for each update
+		self.istream.setperiodsize(self.chunk) #self.chunk / 44100 is how long it will sample for
 		self.istream.setrate(44100)
 		self.istream.setchannels(1)
 		self.colors = [colors.Red, colors.Yellow, colors.Green, colors.Blue]
 
 		
 	def step(self, values, amt = 1):
+		#read from audio input stream
 		dlen, data = self.istream.read()
+		#break data up into array of ints representing audio wave
 		soundarray = ndarray(buffer=data, shape = (self.chunk,), dtype=int16)
 
+		#compute Fast Fourier Transform, 
+		#breaking it into 200 discrete bins (frequencies, 398 / 2 + 1)
 		fft = librosa.core.stft(soundarray, n_fft = 398)
+		#absolute to get power of each frequency
 		sgram = absolute(fft)	
 		value = [0.0, 0.0, 0.0, 0.0]
+		#get average power over the whole time interval
 		bins = average(sgram, axis=1)
-		value[0] = max((bins[0] * 2 / 3, bins[1] * 3 / 4)) #SUB
-		value[1] = max((bins[2] * 8 / 9, bins[3] * 12 /13, bins[4] * 24 / 25)) #Woofer
 
-		value[2] = bins[5:20].max()#midrange
+		#compute four different bars, adjusting for human loudness contours
+		value[0] = max((bins[0] * 2 / 3, bins[1] * 3 / 4)) #SUB (0, 200Hz)
+		value[1] = max((bins[2] * 8 / 9, bins[3] * 12 /13, bins[4] * 24 / 25)) #Woofer (200, 500Hz)
+		value[2] = bins[5:20].max()#midrange (500, 2000Hz)
+		value[3] = bins[20:200].max()#tweeter (2000, 20000Hz)
 
-		value[3] = bins[20:200].max()#tweeter
-
+		#convert power to Bell (volume)
 		value = log10(value) 
 		for y in range(4):
 			if value[y] == float("-inf"):
@@ -61,17 +69,8 @@ class Spectrum(animation.BaseMatrixAnim):
 				db = 1
 			for x in range(db):
 				self._led.set(x, y, self.colors[y])
-#				self._led.set(y, x, self.colors[y])
 			for x in range(db, 12):
 				self._led.set(x, y, colors.Black)
-#				self._led.set(y, x, colors.Black)
-				
-				
-				
-
-		
-			
-
 
 def main():
 	spidriver = SPI(1, 48, spi_speed = 1, c_order = ChannelOrder.BRG, dev = "/dev/spidev0.0", interface = "PERIPHERY")
